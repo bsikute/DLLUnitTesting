21a3b6d917f25235c1aac24f397ea37f80532db9
Brian Sikute <brian.sikute@gmail.com>
2019-12-14 00:05:35 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ConsoleUtilities;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utilities utilities = new Utilities();
+            string stringOutput = "Hello World";
+            utilities.WriteLine(stringOutput);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnitTestConsoleUtilities/UtilitiesTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleUtilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUtilities.Tests
+{
+    [TestClass()]
+    public class UtilitiesTests
+    {
+        [TestMethod()]
+        public void WriteLineTest()
+        {
+            //Arrange
+            Utilities utilities = new Utilities();
+            string stringInputTest = "Hello World";
+
+            //Act
+            string output = utilities.WriteLine(stringInputTest);
+
+            //Assert
+            Assert.AreEqual(stringInputTest, output);
+        }
+    }
+}
